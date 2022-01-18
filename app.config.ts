da29ab1c29dc4f0b2c99f120ab636eb2504642f9
Bruno Mesquita <silvabruno.git@gmail.com>
2022-01-18 20:13:20 -0300

    #feat convertendo arquivo de config do expo para .ts e habilitando hermes

---
+++ app.config.ts
@@ -0,0 +1,36 @@
+import { ExpoConfig } from '@expo/config';
+
+export default {
+  name: 'Flipp - Partners',
+  slug: 'flipp-partners',
+  version: '1.0.0',
+  jsEngine: 'hermes',
+  orientation: 'portrait',
+  icon: './src/assets/icons/logo.png',
+  splash: {
+    image: './src/assets/images/splash.png',
+    resizeMode: 'cover',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'flipp.partners.com',
+  },
+  android: {
+    userInterfaceStyle: 'light',
+    versionCode: 3,
+    googleServicesFile: './google-services.json',
+    adaptiveIcon: {
+      backgroundColor: '#9E0404',
+      foregroundImage: './src/assets/icons/logo.png',
+    },
+    package: 'flipp.partners.com',
+  },
+  extra: {
+    apiUrl: process.env.API_URL ?? 'http://192.168.15.24:3030/api/app-store',
+  },
+  plugins: ['expo-image-picker'],
+} as ExpoConfig;
