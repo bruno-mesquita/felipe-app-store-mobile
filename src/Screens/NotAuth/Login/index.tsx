b44828de3ff3bb46baf0f784528978cbae2c9369
Bruno Mesquita <bmesquita196@gmail.com>
2022-02-25 22:37:13 -0300

    refactor: login com native-base

--- src/Screens/NotAuth/Login/index.tsx
+++ src/Screens/NotAuth/Login/index.tsx
@@ -1,20 +1,21 @@
+import { useState } from 'react';
 import { Alert } from 'react-native';
-import { Formik, FormikHelpers } from 'formik';
+import { Formik, FormikHelpers, ErrorMessage } from 'formik';
+import { Button, FormControl, Input, Flex } from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { useAppDispatch } from '@store/hooks';
 import { authActions } from '@store/reducers/auth';
 
-import { Field, FieldSecure } from '../../../Components/FormUtils';
-import { Button } from '../../../Components';
 import { Layout } from '../_Layout';
-
-import { MyError, Form, ContainerInput } from './styles';
 
 import schema from './schema';
 import type { Values } from './types';
 
 export const Login = ({ navigation }) => {
   const dispatch = useAppDispatch();
+
+  const [visible, setVisible] = useState(false);
 
   const onSubmit = async (
     values: Values,
@@ -40,25 +41,37 @@ export const Login = ({ navigation }) => {
         validationSchema={schema}
       >
         {({ handleSubmit, handleChange, values, isSubmitting }) => (
-          <Form>
-            <ContainerInput>
-              <Field
+          <Flex align="center" px="20px">
+            <FormControl>
+              <FormControl.Label _text={{ color: '#fff' }}>E-mail</FormControl.Label>
+              <Input
                 autoCapitalize="none"
                 onChangeText={handleChange('email')}
                 value={values.email}
                 placeholder="E-mail"
-                label="E-mail"
               />
-              <MyError name="email" />
+              <ErrorMessage name="email" component={FormControl.ErrorMessage} />
+            </FormControl>
 
-              <FieldSecure
+            <FormControl mt="10px">
+              <FormControl.Label _text={{ color: '#fff' }}>Senha</FormControl.Label>
+              <Input
+                secureTextEntry={!visible}
                 onChangeText={handleChange('password')}
                 value={values.password}
                 placeholder="Senha"
-                label="Senha"
+                rightElement={
+                  <MaterialCommunityIcons
+                    onPress={() => setVisible(!visible)}
+                    size={25}
+                    color="#fff"
+                    style={{ marginRight: 10 }}
+                    name={visible ? 'eye' : 'eye-off'}
+                  />
+                }
               />
-              <MyError name="password" />
-            </ContainerInput>
+              <ErrorMessage name="password" component={FormControl.ErrorMessage} />
+            </FormControl>
 
             {/* <ForgotPassword>
               <ForgotPasswordButton>
@@ -66,13 +79,15 @@ export const Login = ({ navigation }) => {
               </ForgotPasswordButton>
             </ForgotPassword> */}
             <Button
-              style={{ marginTop: 20 }}
-              loading={isSubmitting}
+              w="50%"
+              mt="50px"
+              isLoading={isSubmitting}
+              isDisabled={isSubmitting}
               onPress={() => handleSubmit()}
             >
               Login
             </Button>
-          </Form>
+          </Flex>
         )}
       </Formik>
     </Layout>
