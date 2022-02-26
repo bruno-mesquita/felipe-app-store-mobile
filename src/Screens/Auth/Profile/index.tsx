a28085ccf8c57365ab1416b77e79a917039f98e4
Bruno Mesquita <bmesquita196@gmail.com>
2022-02-25 22:25:23 -0300

    refactor: tela de perfil do dono com native base

--- src/Screens/Auth/Profile/index.tsx
+++ src/Screens/Auth/Profile/index.tsx
@@ -1,103 +1,98 @@
 import { useRef } from 'react';
-import { Alert, ScrollView } from 'react-native';
-import { Formik, FormikHelpers } from 'formik';
+import { ErrorMessage, Formik } from 'formik';
 import { TextInputMasked } from 'react-native-masked-text';
+import { Flex, Button, useToast, FormControl, Input } from 'native-base';
 
 import api from '@services/api';
 import { useUser } from '../../../hooks';
-import { Field, FieldMask, FieldError } from '../../../Components/FormUtils';
-import { Button } from '../../../Components';
+import { FieldMask } from '../../../Components/FormUtils';
 
-import { Container, Form } from './styles';
 import { Values } from './props';
 import schema from './schema';
 
 export const Profile = () => {
+  const toast = useToast();
+
   const user = useUser(['last_name', 'first_name', 'email', 'cellphone']);
   const inputPhoneRef = useRef<TextInputMasked>(null);
 
-  const onSubmit = async (
-    values: Values,
-    { setSubmitting }: FormikHelpers<Values>
-  ) => {
+  const onSubmit = async (values: Values) => {
     try {
-      const body = {
-        first_name: values.first_name,
-        last_name: values.last_name,
-        email: values.email,
+      await api.put('/owners', {
+        ...values,
         cellphone: inputPhoneRef.current?.getRawValue(),
-      };
+      });
 
-      await api.put('/owners', body);
-      Alert.alert('Sucesso', 'Dados atualizados com sucesso :)');
+      toast.show({
+        title: 'Sucesso',
+        description: 'Dados atualizados com sucesso :)',
+      });
     } catch (err) {
-      Alert.alert(
-        'Erro',
-        'Parece que houve um erro ao atualizar os seus dados :('
-      );
-    } finally {
-      setSubmitting(false);
+      toast.show({
+        title: 'Erro',
+        description: 'Parece que houve um erro ao atualizar os seus dados :(',
+      });
     }
   };
 
   return (
-    <ScrollView>
-      <Container>
-        <Formik
-          initialValues={user}
-          onSubmit={onSubmit}
-          enableReinitialize
-          validationSchema={schema}
-        >
-          {({ values, handleChange, handleSubmit, isSubmitting }) => (
-            <Form>
-              <Field
-                labelColor="#000"
-                label="Primeiro nome"
-                onChangeText={handleChange('first_name')}
-                value={values.first_name}
-              />
-              <FieldError name="first_name" />
+    <Flex flex={1} justify="space-around" px="25px">
+      <Formik
+        initialValues={user}
+        onSubmit={onSubmit}
+        enableReinitialize
+        validationSchema={schema}
+      >
+        {({ values, handleChange, handleSubmit, isSubmitting }) => (
+          <>
+            <Flex>
+              <FormControl>
+                <FormControl.Label>Primeiro nome</FormControl.Label>
+                <Input
+                  onChangeText={handleChange('first_name')}
+                  value={values.first_name}
+                />
+                <ErrorMessage name="first_name" component={FormControl.ErrorMessage} />
+              </FormControl>
 
-              <Field
-                labelColor="#000"
-                label="Sobbrenome"
-                onChangeText={handleChange('last_name')}
-                value={values.last_name}
-              />
-              <FieldError name="last_name" />
+              <FormControl mt="10px">
+                <FormControl.Label>Sobrenome</FormControl.Label>
+                <Input
+                  onChangeText={handleChange('last_name')}
+                  value={values.last_name}
+                />
+                <ErrorMessage name="last_name" component={FormControl.ErrorMessage} />
+              </FormControl>
 
-              <Field
-                labelColor="#000"
-                label="Email"
-                onChangeText={handleChange('email')}
-                value={values.email}
-              />
-              <FieldError name="email" />
+              <FormControl mt="10px">
+                <FormControl.Label>Email</FormControl.Label>
+                <Input onChangeText={handleChange('email')} value={values.email} />
+                <ErrorMessage name="email" component={FormControl.ErrorMessage} />
+              </FormControl>
 
-              <FieldMask
-                maskRef={inputPhoneRef}
-                labelColor="#000"
-                type="cel-phone"
-                label="Telefone"
-                options={{ withDDD: true }}
-                onChangeText={handleChange('cellphone')}
-                value={values.cellphone}
-              />
-              <FieldError name="cellphone" />
-
-              <Button
-                disabled={isSubmitting}
-                loading={isSubmitting}
-                style={{ marginTop: 20 }}
-                onPress={() => handleSubmit()}
-              >
-                Atualizar
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Container>
-    </ScrollView>
+              <FormControl mt="10px">
+                <FormControl.Label>Telefone</FormControl.Label>
+                <FieldMask
+                  maskRef={inputPhoneRef}
+                  type="cel-phone"
+                  options={{ withDDD: true }}
+                  onChangeText={handleChange('cellphone')}
+                  value={values.cellphone}
+                />
+                <ErrorMessage name="cellphone" component={FormControl.ErrorMessage} />
+              </FormControl>
+            </Flex>
+            <Button
+              mt="20px"
+              isDisabled={isSubmitting}
+              isLoading={isSubmitting}
+              onPress={() => handleSubmit()}
+            >
+              Atualizar
+            </Button>
+          </>
+        )}
+      </Formik>
+    </Flex>
   );
 };
