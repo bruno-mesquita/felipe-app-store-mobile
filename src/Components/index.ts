6241f0a579767f7c064f7a438bc2e1f41248ef7e
Bruno Mesquita <bmesquita196@gmail.com>
2022-02-25 23:10:43 -0300

    fix: import nao existente

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -4,7 +4,6 @@ export { AppLoading } from './AppLoading';
 export { MenuForm } from './MenuForm';
 export { ProductForm } from './ProductForm';
 export { ModalBase } from './ModalBase';
-export { FloatButton } from './FloatButton';
 export * from './_Bases';
 export { Checkbox } from './Checkbox';
 export { ButtonModal } from './ButtonModal';
