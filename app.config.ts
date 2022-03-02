7596d61237a369d042bb4592992175b16996ef12
Bruno Mesquita <bmesquita196@gmail.com>
2022-03-01 22:39:58 -0300

    feature: versionCode 8

--- app.config.ts
+++ app.config.ts
@@ -1,34 +1,10 @@
-import { ExpoConfig } from '@expo/config';
+import type { ExpoConfig, ConfigContext } from '@expo/config';
 
-export default {
+export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'Flipp - Partners',
   slug: 'flipp-partners',
-  version: '1.0.0',
-  jsEngine: 'hermes',
-  orientation: 'portrait',
-  icon: './src/assets/icons/logo.png',
-  splash: {
-    image: './src/assets/images/splash.png',
-    resizeMode: 'cover',
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
-  assetBundlePatterns: ['**/*'],
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: 'com.partners.flipp',
-  },
-  android: {
-    versionCode: 7,
-    googleServicesFile: './google-services.json',
-    adaptiveIcon: {
-      backgroundColor: '#9E0404',
-      foregroundImage: './src/assets/icons/logo.png',
-    },
-    package: 'com.partners.flipp',
-  },
+  ...config,
   extra: {
     apiUrl: process.env.API_URL ?? 'http://192.168.15.24:3030/api/app-store',
   },
-} as ExpoConfig;
+});
